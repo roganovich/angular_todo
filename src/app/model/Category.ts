76eca4d512a2129b0790f68459d1d7445828790a
Roman Roganovich (Soft) <roman.roganovich@mail.ru>
2021-01-11 18:19:20 +0300

    upload test data

---
+++ src/app/model/Category.ts
@@ -0,0 +1,14 @@
+/**
+* @author Roganovich R.M.
+*/
+
+export class Categori {
+  id: number;
+  title: string;
+
+  constructor (id:number, title:string){
+    this.id = idea;
+    this.title = title;
+
+  }
+}
